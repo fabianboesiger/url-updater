b6421ee3a0b605323ad337de2afd0102dd9c4dc6
fabianboesiger <boesiger.fabian@outlook.com>
2018-11-04 20:43:40 +0100

    Some Changes

--- UrlUpdater/src/Main.java
+++ UrlUpdater/src/Main.java
@@ -1,25 +1,37 @@
 import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
+import java.net.ProtocolException;
 import java.net.URL;
 
 public class Main {
+	
+	static URL url;
+	static HttpURLConnection connection;
 	
 	public static void main(String[] args) {
 		
 		final String URL = args[0];
 		final int UPDATES_PER_DAY = Integer.parseInt(args[1]);
 		
+		try{
+			url = new URL(URL);
+		}catch(MalformedURLException e){
+			e.printStackTrace();
+		}
+		
+		try{
+			connection = (HttpURLConnection) url.openConnection();
+			connection.setRequestMethod("GET");
+			connection.setRequestProperty("User-Agent", "Mozilla/5.0");
+		}catch(IOException e){
+			e.printStackTrace();
+		}
+		
 		while(true){
 
 			try{
-				URL url = new URL(URL);
-				HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-				connection.setRequestMethod("GET");
-				connection.setRequestProperty("User-Agent", "Mozilla/5.0");
 				System.out.println("Accessed "+URL+" with response code "+connection.getResponseCode());
-			}catch(MalformedURLException e){
-				e.printStackTrace();
 			}catch(IOException e){
 				e.printStackTrace();
 			}
